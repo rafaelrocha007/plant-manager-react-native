9aa88565a22c070201152af5fee91bbbd06167a0
Rafael Rocha <rafaelcosta@cloudfox.net>
2021-04-24 06:59:15 -0300

    refactor: remove unused state variables

--- src/pages/PlantSelect/index.tsx
+++ src/pages/PlantSelect/index.tsx
@@ -36,7 +36,6 @@ export function PlantSelect() {
   const [loading, setLoading] = useState(true)
   const [page, setPage] = useState(1)
   const [loadingMore, setLoadingMore] = useState(false)
-  const [loadedAll, setLoadedAll] = useState(false)
 
   async function fetchEnvironment() {
     const { data } = await api.get(`plants_environments?_sort=title&_order=asc`)

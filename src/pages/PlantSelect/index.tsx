b52366c6d05033f18dd44a13b318977173af02b0
Rafael Rocha <rafaelcosta@cloudfox.net>
2021-04-24 06:58:12 -0300

    refactor: set key prop to FlatList

--- src/pages/PlantSelect/index.tsx
+++ src/pages/PlantSelect/index.tsx
@@ -107,6 +107,7 @@ export function PlantSelect() {
       <View>
         <FlatList
           data={environments}
+          keyExtractor={(item) => String(item.key)}
           renderItem={({ item }) => (
             <EnvironmentButton
               title={item.title}
@@ -123,6 +124,7 @@ export function PlantSelect() {
       <View style={styles.plants}>
         <FlatList
           data={filteredPlants}
+          keyExtractor={(item) => String(item.id)}
           renderItem={({ item }) => (
             <PlantCardPrimary data={item} />
           )}
